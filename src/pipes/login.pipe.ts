4315f5daef133d7272ea299a8aae9e0356c9c8f6
Hansanguk <hansanguk0222@gmail.com>
2021-10-02 12:12:52 +0900

    [feat] 로그인 body에서 빈 값을 체크하는 파이프 추가

---
+++ src/pipes/login.pipe.ts
@@ -0,0 +1,21 @@
+import { emptyValueCheck } from '@/utils/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+
+@Injectable()
+export class LoginPipe implements PipeTransform {
+  transform({ email, nickname }: { email: string; nickname: string }) {
+    console.log(email, nickname, emptyValueCheck([email, nickname]));
+    if (emptyValueCheck([email, nickname])) {
+      throw new HttpException(
+        '빈 값이 있어요 확인해주세요!',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return { email, nickname };
+  }
+}
